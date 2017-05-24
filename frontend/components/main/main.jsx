32931c380533ed8f11364967a9605d9650d93473
mixmastert <torahlight@gmail.com>
2017-05-24 14:29:24 -0500

    add media query to Main component to hide Map when screen size is less than 640px. Add button to show/hide map. Still needs styling

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -7,14 +7,28 @@ import Networks from '../networks/networks_container';
 class Main extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      mapShowing: false,
+    }
+  }
+
+  componentDidMount() {
+    if (window.matchMedia("(min-width: 640px)").matches) {
+      console.log("screen width is greater than 640 px!");
+      this.setState({ mapShowing: true })
+    }
   }
 
   render() {
     return (
       <main className='main'>
         <Locations />
-        <Map />
+        { this.state.mapShowing ? <Map /> : "" }
         <Networks />
+        <button onClick={() => this.setState({ mapShowing: !this.state.mapShowing })}>
+          { this.state.mapShowing ? "Hide " : "Show "}Map
+        </button>
       </main>
     );
   }
