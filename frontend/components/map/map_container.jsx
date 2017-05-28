13261dac45c31651809d6eefa007878c54ec0508
mixmastert <torahlight@gmail.com>
2017-05-28 15:11:25 -0500

    add clickHandler to set networkDetail for network icon click on map as for network name click on Network component

--- frontend/components/map/map_container.jsx
+++ frontend/components/map/map_container.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Map from './map.jsx';
 
-import { getNetworks } from '../../actions/network_actions';
+import { getNetworks, getNetwork } from '../../actions/network_actions';
 import { updateBounds } from '../../actions/bounds_actions';
 
 const mapStateToProps = (state, ownProps) => ({
   city: state.city,
   country: state.country,
   nearbyNetworks: state.nearbyNetworks,
-  networkDetail: state.networkDetail.network, 
+  networkDetail: state.networkDetail.network,
 });
 
 const mapDispatchToProps = dispatch => ({
   getNetworks: () => dispatch(getNetworks()),
-  updateBounds: bounds => dispatch(updateBounds(bounds))
+  getNetwork: (networkId) => dispatch(getNetwork(networkId)),
+  updateBounds: bounds => dispatch(updateBounds(bounds)),
 });
 
 export default connect(
