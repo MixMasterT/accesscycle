dd36b74b967695ec274aae0bb92a78dc36fb8939
mixmastert <torahlight@gmail.com>
2017-06-01 23:55:31 -0500

    Add form showing basic info on station component, and test to render a simple h2 statement when no station is selected

--- frontend/components/station/station.jsx
+++ frontend/components/station/station.jsx
@@ -2,11 +2,37 @@ import React from 'react';
 
 const Station = ({ station }) => {
   console.log(station);
-  return (
-    <div>
-      <h2>Station</h2>
-    </div>
-  );
+  if (Object.keys(station).length === 0) {
+    return (
+      <h2>No Station Selected</h2>
+    )
+  } else {
+    return (
+      <div className='station'>
+        <h2>Station: {station.name}</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>{station.address}</th>
+            </tr>
+
+            <tr>
+              <th>bikes available</th><th>free slots</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>{station.free_bikes}</td><td>{station.empty_slots}</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+    );
+
+  }
+
 }
 
 
