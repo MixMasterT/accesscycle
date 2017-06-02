3b5c3b96ae24451021dfbc8c451f49fac183d4b0
mixmastert <torahlight@gmail.com>
2017-06-02 11:50:42 -0500

    clear station slice of state when new location is chosen by modifying stationReducer

--- frontend/reducers/station_reducer.js
+++ frontend/reducers/station_reducer.js
@@ -4,6 +4,9 @@ import {
          RECEIVE_STATION,
          CLEAR_STATION } from "../actions/station_actions.js";
 
+import { SET_CITY } from "../actions/city_actions.js";
+import { SET_COUNTRY } from "../actions/country_actions.js";
+
 let _defaultState = {};
 
 const stationReducer = (state = _defaultState, action) => {
@@ -12,6 +15,8 @@ const stationReducer = (state = _defaultState, action) => {
     case RECEIVE_STATION:
       return action.station;
     case CLEAR_STATION:
+    case SET_CITY:
+    case SET_COUNTRY:
       return _defaultState;
     default:
       return state;
